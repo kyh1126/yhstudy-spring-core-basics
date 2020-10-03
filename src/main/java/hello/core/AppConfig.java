3d4187b994bcdd2638a41489d5e2ad3c1fe04902
kyh1126 <kyh901126@nate.com>
2020-10-04 01:24:37 +0900

    [싱글톤 컨테이너] ~@Configuration과 바이트코드 조작의 마법

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,16 +16,22 @@ public class AppConfig {
 
 	@Bean
 	public MemberService memberService() {
+		//1번
+		System.out.println("call AppConfig.memberService");
 		return new MemberServiceImpl(memberRepository());
 	}
 
 	@Bean
 	public OrderService orderService() {
+		//1번
+		System.out.println("call AppConfig.orderService");
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
 	@Bean
 	public MemoryMemberRepository memberRepository() {
+		//2번? 3번?
+		System.out.println("call AppConfig.memberRepository");
 		return new MemoryMemberRepository();
 	}
 
