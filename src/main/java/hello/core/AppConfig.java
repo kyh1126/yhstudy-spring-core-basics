29455272e0d556f4fc891fdfdd7a74af29e9ef91
kyh1126 <kyh901126@nate.com>
2020-10-03 23:42:36 +0900

    [객체 지향 원리 적용] IoC, DI, 그리고 컨테이너

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,7 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +11,19 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
 	}
 
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
+	}
+
+	private MemoryMemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+
+	private DiscountPolicy discountPolicy() {
+//		return new FixDiscountPolicy();
+		return new RateDiscountPolicy();
 	}
 }
