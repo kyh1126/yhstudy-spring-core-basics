4d916464f5d87e5a9b918cb4804d17878e02ef27
kyh1126 <kyh901126@nate.com>
2020-10-04 15:09:19 +0900

    [빈 생명주기 콜백] 빈 생명주기 콜백 시작

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,27 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+class BeanLifeCycleTest {
+	@Test
+	public void lifeCycleTest() {
+		ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+		NetworkClient client = ac.getBean(NetworkClient.class);
+		ac.close(); //스프링 컨테이너를 종료, ConfigurableApplicationContext 필요
+	}
+
+	@Configuration
+	static class LifeCycleConfig {
+		@Bean
+		public NetworkClient networkClient() {
+			NetworkClient networkClient = new NetworkClient();
+			// 객체가 생성된 후 수정자 주입을 통해 값이 들어올 수도 있다.
+			networkClient.setUrl("http://hello-spring.dev");
+			return networkClient;
+		}
+	}
+}
