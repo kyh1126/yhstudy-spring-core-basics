46b93518b0cce96cdb9e72c56d48322a0e493c1a
kyh1126 <kyh901126@nate.com>
2020-10-04 16:46:26 +0900

    [프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점] 스코프와 Provider

--- src/main/java/hello/core/logdemo/LogDemoService.java
+++ src/main/java/hello/core/logdemo/LogDemoService.java
@@ -2,6 +2,7 @@ package hello.core.logdemo;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 import hello.core.common.MyLogger;
@@ -10,9 +11,10 @@ import hello.core.common.MyLogger;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerProvider;
 
 	public void logic(String id) {
+		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.log("service id = " + id);
 	}
 }
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,12 +17,13 @@ import hello.core.logdemo.LogDemoService;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerProvider;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
+		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.setRequestURL(requestURL);
 
 		myLogger.log("controller test");
