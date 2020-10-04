7dd0f0fa729cb1b13bb5847bca5038f1767a52c7
kyh1126 <kyh901126@nate.com>
2020-10-04 16:59:58 +0900

    [프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점] 스코프와 프록시

--- src/main/java/hello/core/logdemo/LogDemoService.java
+++ src/main/java/hello/core/logdemo/LogDemoService.java
@@ -11,10 +11,9 @@ import hello.core.common.MyLogger;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-	private final ObjectProvider<MyLogger> myLoggerProvider;
+	private final MyLogger myLogger;
 
 	public void logic(String id) {
-		MyLogger myLogger = myLoggerProvider.getObject();
 		myLogger.log("service id = " + id);
 	}
 }
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -17,13 +17,13 @@ import hello.core.logdemo.LogDemoService;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final ObjectProvider<MyLogger> myLoggerProvider;
+	private final MyLogger myLogger;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
-		MyLogger myLogger = myLoggerProvider.getObject();
+		System.out.println("myLogger = " + myLogger.getClass());
 		myLogger.setRequestURL(requestURL);
 
 		myLogger.log("controller test");
